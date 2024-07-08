01d887fffaab891c304c608c2c58b8ba1a7b57e4
ElenaKanash <hird250@gmail.com>
2024-07-08 19:36:44 +0300

    add step for US_11.01.07_Menu [Educations]

---
+++ pages/elements/blockStepTrading.js
@@ -0,0 +1,15 @@
+class BlockStepTrading {
+  constructor(page){
+    this.page = page;
+
+    //this.getCreateAndVerifyYourAccountIcon = page.locator('.regSteps__shape > i[data-type="banner_with_steps"]');
+    //this.getCreateAndVerifyYourAccountText = page.locator('`.regSteps__shape > b:has-text("${text}")`')
+    this.getCreateAndVerifyYourAccountBtn = page.locator('.regSteps__shape > i[data-type="banner_with_steps"], .regSteps__shape > b')
+  }
+
+  async clickCreateAndVerifyYourAccountBtn() {
+    await this.getCreateAndVerifyYourAccountBtn.click();
+  }
+
+}
+export default BlockStepTrading
