ae5d8b51da175937e2c6ce3c751c74f1190a3718
ElenaKanash <hird250@gmail.com>
2024-07-08 17:59:27 +0300

    refactor

--- pages/login.js
+++ pages/login.js
@@ -13,7 +13,7 @@ class Login {
     this.getPasswordField = page.getByRole('textbox', { name: 'Password' });
     this.getContinueBtn = page.getByRole('button', { name: 'Continue' });
     this.getMyAccountButton = page.locator('header').getByRole('button', { name: 'My account' });
-    this.getLogout = page.locator('.logout-user')//page.getByText('Logout')
+    this.getLogoutBtn = page.locator('.logout-user')//page.getByText('Logout')
 
   }
 
@@ -34,11 +34,13 @@ class Login {
     await this.page.waitForNavigation();
     await this.page.goBack();
     await this.page.waitForLoadState('load');
-    await this.getMyAccountButton.scrollIntoViewIfNeeded();
     await this.checkMyAccountButton();
+    await this.getMyAccountButton.scrollIntoViewIfNeeded();
     await this.getMyAccountButton.focus();
     await this.clickMyAcoountBtn();
-    await this.getLogout.scrollIntoViewIfNeeded();
+    await this.checkLogoutBtn();
+    await this.getLogoutBtn.scrollIntoViewIfNeeded();
+    await this.getLogoutBtn.focus();
     await this.clickLogoutBtn();
   }
 
@@ -72,11 +74,15 @@ class Login {
   }
 
   async clickMyAcoountBtn() {
-    await this.getMyAccountButton.click();
+    await this.getMyAccountButton.click({ timeout: 15000 });
+  }
+
+  async checkLogoutBtn() {
+    await expect(this.getLogoutBtn).toBeVisible();
   }
 
   async clickLogoutBtn() {
-    await this.getLogout.click();
+    await this.getLogoutBtn.click({ timeout: 15000 });
     await expect(this.header.getLoginButton).toBeVisible();
   }
 
